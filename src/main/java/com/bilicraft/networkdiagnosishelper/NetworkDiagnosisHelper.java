ad41d1f662f405cbb5a0ba653451899c3721bd98
Ghost_chu <2908803755@qq.com>
2021-07-03 16:06:06 +0800

    Added getter

--- src/main/java/com/bilicraft/networkdiagnosishelper/NetworkDiagnosisHelper.java
+++ src/main/java/com/bilicraft/networkdiagnosishelper/NetworkDiagnosisHelper.java
@@ -1,32 +1,29 @@
 package com.bilicraft.networkdiagnosishelper;
 
 import com.google.gson.Gson;
-import jdk.vm.ci.hotspot.EventProvider;
-import net.md_5.bungee.api.connection.Connection;
+import lombok.Getter;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
 import net.md_5.bungee.api.event.PluginMessageEvent;
-import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.event.EventHandler;
 import net.md_5.bungee.event.EventPriority;
 
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public final class NetworkDiagnosisHelper extends Plugin implements Listener {
-    private Set<ProxiedPlayer> modInstalledPlayers = new HashSet<>();
+    @Getter
+    private final Set<ProxiedPlayer> modInstalledPlayers = new HashSet<>();
     private final Gson gson = new Gson();
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         getProxy().registerChannel("networkdiagnosis:command");
-        getProxy().getPluginManager().registerListener(this,this);
+        getProxy().getPluginManager().registerListener(this, this);
     }
 
     @Override
@@ -36,22 +33,24 @@ public final class NetworkDiagnosisHelper extends Plugin implements Listener {
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onDisconnect(PlayerDisconnectEvent event){
+    public void onDisconnect(PlayerDisconnectEvent event) {
         this.modInstalledPlayers.remove(event.getPlayer());
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onMessage(PluginMessageEvent event) {
-        if(event.isCancelled()){
+        if (event.isCancelled()) {
             return;
         }
         try {
             if (event.getTag().equals("minecraft:register")) {
                 String registering = new String(event.getData(), StandardCharsets.UTF_8);
-                if (registering.contains("networkdiagnosis:command")) {
-                    if (event.getSender() instanceof ProxiedPlayer) {
+                if (event.getSender() instanceof ProxiedPlayer) {
+                    if (registering.contains("networkdiagnosis:command")) {
                         getLogger().info("Player " + ((ProxiedPlayer) event.getSender()).getName() + " have NetworkDiagnosis mod, feature enabled.");
                         this.modInstalledPlayers.add((ProxiedPlayer) event.getSender());
+                    }else{
+                        getLogger().info("Player " + ((ProxiedPlayer) event.getSender()).getName() + " had no NetworkDiagnosis mod installed, feature disabled.");
                     }
                 }
             }
@@ -59,16 +58,16 @@ public final class NetworkDiagnosisHelper extends Plugin implements Listener {
                 ResponseContainer responseContainer = gson.fromJson(new String(event.getData(), StandardCharsets.UTF_8), ResponseContainer.class);
                 processResponse(responseContainer);
             }
-        }catch (Exception exception){
+        } catch (Exception exception) {
             exception.printStackTrace();
         }
     }
 
-    private void processResponse(ResponseContainer responseContainer){
-        // TODO: Storage test results.
+    private void processResponse(ResponseContainer responseContainer) {
+
     }
 
-    public void sendTestRequest(ProxiedPlayer player, CommandContainer commandContainer){
-        player.sendData("networkdiagnosis:command",gson.toJson(commandContainer).getBytes(StandardCharsets.UTF_8));
+    public void sendTestRequest(ProxiedPlayer player, CommandContainer commandContainer) {
+        player.sendData("networkdiagnosis:command", gson.toJson(commandContainer).getBytes(StandardCharsets.UTF_8));
     }
 }
